8aae52b0f82599edb1bd070e4d71717393d3a945
kangalio <kangalio@users.noreply.github.com>
2023-11-02 18:02:16 +0000

    deploy: 8e917d226e69a075b0f5981336ac9eb37436e1f0

---
+++ current/trait.impl/futures_util/future/future/trait.FutureExt.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"futures":[],
+"futures_util":[],
+"serenity":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
